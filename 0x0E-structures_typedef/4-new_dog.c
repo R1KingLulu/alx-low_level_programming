52e3e76251ba3e013c4f41870d991810c912988a
R1KingLulu <lulu23836@gmail.com>
2023-08-14 14:23:09 -0700

    c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include "dog.h"
 
-
 /**
 * new_dog - creates a new dog
 * @name: name of dog
@@ -14,8 +13,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int nl, ol, i;
 	dog_t *dog;
-
-
 	if (name == NULL || owner == NULL)
 	return (NULL);
 	dog = malloc(sizeof(dog_t));
