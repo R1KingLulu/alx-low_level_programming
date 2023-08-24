0562aae325693705b95a61fca8c0963dfbecd7f8
R1KingLulu <lulu23836@gmail.com>
2023-08-24 12:34:35 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
  * File: lists.h
  * Auth: Enough B Mkhabela
  * Desc: Header file containing prototypes and definitions for all the functions
- *       and types written in the 0x11-singly_linked_lists directory.
+ * and types written in the 0x11-singly_linked_lists directory.
  */
 #include <stdlib.h>
 
@@ -25,11 +25,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _putchar(char c)
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* LISTS_H */
