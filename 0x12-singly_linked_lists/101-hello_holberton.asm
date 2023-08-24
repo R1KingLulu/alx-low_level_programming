81cf463a04c0dbc3760e094f56d6ef2acc5b5808
R1KingLulu <lulu23836@gmail.com>
2023-08-24 12:26:57 -0700

    correct

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,29 @@
-extern printf; the C function, to be called
+; File: 101-hello_holberton.asm
+; Auth: Enough B Mkhabela
+; Desc: 64-bit assembly program that prints
+; Hello, Holberton followed by a new line.
 
+section .data
+    hello db "Hello, Holberton", 0
+    format db "%s", 10, 0 ; %s is the format specifier for a null-terminated string, 10 is the ASCII code for newline
 
-      section.data; Data section, initialized variables
-msg:    db "Hello, Holberton", 0; C string needs 0
-fmt:    db "%s", 10, 0; The printf format, "\n", '0'
+section .text
+    global main
+    extern printf
 
+main:
+    ; Call printf with the format string and the address of the hello string
+    mov rdi, format
+    mov rsi, hello
+    xor rax, rax ; Clear RAX to indicate we're using the cdecl calling convention
+    call printf
 
-       section.text; Code section.
+    ; Exit the program
+    mov rdi, 0
+    call exit
 
-
-   global main; the standard gcc entry point
-   main:; the program label for the entry point
-   push    rbp     ; set up stack frame, must be alligned
-
-
-   mov rdi,fmt
-   mov rsi,msg
-   mov rax,0; or can be xor rax, rax
+exit:
+    ; Call the exit system call
+    mov rax, 60 ; syscall number for exit
+    xor rdi, rdi ; exit status 0
+    syscall
