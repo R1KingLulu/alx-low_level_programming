dcf3fd112f7252ed49e93c81d2b0af7716f38fa9
R1KingLulu <lulu23836@gmail.com>
2023-09-04 03:47:47 -0700

    done

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/* prototypes of functions */
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int error_handler(int fd, char *fname, int type, char *buffer);
+char *create_buffer(char *fileName);
+void close_file(int fd);
+
+#endif
